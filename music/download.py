1ccbbfb8214a348018225f4b66341e243c15f3a4
jaxsus <juho@codemind.co.kr>
2019-08-22 13:13:21 +0900

    update - check subprocess waiting for download video

--- music/download.py
+++ music/download.py
@@ -1,18 +1,14 @@
-import os
 import subprocess
 from time import sleep
 import DAO_youtube as utub
 
 rows = utub.get_undownloaded_videos()
+cmd = 'youtube-dl --audio-format mp3 --audio-quality 0 --output "mp3/%(title)s.%(ext)s" -x {}'
 
 for row in rows:
     id = row[utub.idx_id]
-    title = row[utub.idx_title]
     uri = row[utub.idx_uri]
-    subprocess.call('youtube-dl --audio-format mp3 --audio-quality 0 --output "mp3/{}.mp3" -x {}'.format(title, uri),
-                    shell=True)
-    while not os.path.isfile('mp3/{}.mp3'.format(title)) or os.path.isfile('mp3/{}.webm'.format(title)):
-        print('wait for {}'.format(title))
-        sleep(3)
+    subprocess.call(cmd.format(uri), shell=True)
     utub.update_download_video(id)
     sleep(5)
+
